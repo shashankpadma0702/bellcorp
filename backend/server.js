116c08bee92c0f4adaa617a6b91cb53bca01746f
shashank padma <shashankpadma07@gmail.com>
2026-02-16 00:40:57 +0530

    Downgraded to Express 4 to fix wildcard route

--- backend/server.js
+++ backend/server.js
@@ -26,12 +26,10 @@ app.use("/api/transactions", transactionRoutes);
 // 🔥 Serve React Frontend
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("/(.*)", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
